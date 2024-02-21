8c2455d74d5d3d434263ecfc75cbbcd357e9503e
Murat Artan <muratartan@hotmail.com>
2024-02-21 23:47:28 +0300

    config metadata.ts

--- ts/cluster1/spl_metadata.ts
+++ ts/cluster1/spl_metadata.ts
@@ -1,48 +1,68 @@
-import wallet from "../wba-wallet.json"
-import { createUmi } from "@metaplex-foundation/umi-bundle-defaults"
-import { 
-    createMetadataAccountV3, 
-    CreateMetadataAccountV3InstructionAccounts, 
-    CreateMetadataAccountV3InstructionArgs,
-    DataV2Args
+import wallet from "../wba-wallet.json";
+import { createUmi } from "@metaplex-foundation/umi-bundle-defaults";
+import {
+  createMetadataAccountV3,
+  CreateMetadataAccountV3InstructionAccounts,
+  CreateMetadataAccountV3InstructionArgs,
+  DataV2Args,
+  MPL_TOKEN_METADATA_PROGRAM_ID,
 } from "@metaplex-foundation/mpl-token-metadata";
-import { createSignerFromKeypair, signerIdentity, publicKey } from "@metaplex-foundation/umi";
+import {
+  createSignerFromKeypair,
+  signerIdentity,
+  publicKey,
+} from "@metaplex-foundation/umi";
+import { PublicKey } from "@solana/web3.js";
 
 // Define our Mint address
-const mint = publicKey("<mint address>")
+const mint = publicKey("6p1Ad8EgNr11GAe1J8v1cGQBhmMNHiNAvKBjn9X5MJRW");
 
 // Create a UMI connection
-const umi = createUmi('https://api.devnet.solana.com');
+const umi = createUmi("https://api.devnet.solana.com");
 const keypair = umi.eddsa.createKeypairFromSecretKey(new Uint8Array(wallet));
 const signer = createSignerFromKeypair(umi, keypair);
 umi.use(signerIdentity(createSignerFromKeypair(umi, keypair)));
 
 (async () => {
-    try {
-        // Start here
-        // let accounts: CreateMetadataAccountV3InstructionAccounts = {
-        //     ???
-        // }
+  try {
+    // Start here
+    const [metadata] = PublicKey.findProgramAddressSync(
+      [
+        Buffer.from("metadata"),
+        new PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID).toBuffer(),
+        new PublicKey(mint).toBuffer(),
+      ],
+      new PublicKey(MPL_TOKEN_METADATA_PROGRAM_ID)
+    );
 
-        // let data: DataV2Args = {
-        //     ???
-        // }
-
-        // let args: CreateMetadataAccountV3InstructionArgs = {
-        //     ???
-        // }
-
-        // let tx = createMetadataAccountV3(
-        //     umi,
-        //     {
-        //         ...accounts,
-        //         ...args
-        //     }
-        // )
-
-        // let result = await tx.sendAndConfirm(umi).then(r => r.signature.toString());
-        // console.log(result);
-    } catch(e) {
-        console.error(`Oops, something went wrong: ${e}`)
-    }
+    let accounts: CreateMetadataAccountV3InstructionAccounts = {
+      metadata: publicKey(metadata),
+      mint,
+      mintAuthority: signer,
+    };
+    let data: DataV2Args = {
+      name: "Reiner Braun",
+      symbol: "RB",
+      uri: "",
+      sellerFeeBasisPoints: 500,
+      creators: null,
+      collection: null,
+      uses: null,
+    };
+    let args: CreateMetadataAccountV3InstructionArgs = {
+      data,
+      isMutable: true,
+      collectionDetails: null,
+    };
+    let tx = createMetadataAccountV3(umi, {
+      ...accounts,
+      ...args,
+    });
+    let result = await tx
+      .sendAndConfirm(umi)
+      .then((r) => r.signature.toString());
+    console.log(result);
+  } catch (e) {
+    console.error(`Oops, something went wrong: ${e}`);
+  }
 })();
